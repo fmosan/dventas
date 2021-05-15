034c0c2a1aa768cbbcc77ff82313ef24dfe480f5
Francisco Morales Sánchez <fcomorales.sanchez@gmail.com>
2021-05-15 17:10:14 -0400

    vista crear compra

--- resources/views/compra/create.blade.php
+++ resources/views/compra/create.blade.php
@@ -1,47 +1,54 @@
 @extends('adminlte::page')
 
 @section('title', 'Sistema de Ventas')
-
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
 @section('content_header')
     <h1>Registrar Compra</h1>
 @stop
 
 @section('content')
 
-    <form action="/detalle_compra" method="POST">
+    <form action="{{ route('compra.store') }}" method="POST">
         @csrf
         <div class="card-body">
             <div class="row">
                 <div class="col-md-6">
                     <div class="form-group">
-                        <label for="" class="form-label">Proveedor</label>
-                        <input id="proveedor" name="proveedor" type="text" class="form-control" tabindex="1" >
+                        <label for="idproveedor" class="form-label">Proveedor</label>
+                        <select class="form-control" name="idproveedor">
+                            <option selected>Seleccionar</option>
+                            @foreach ($proveedores as $proveedor)
+                                <option value="{{$proveedor->id}}">{{$proveedor->persona->nombre}}</option>
+                            @endforeach
+                        </select>
                     </div>
                     <div class="form-group">
-                        <label for="" class="form-label">Impuesto</label>
+                        <label for="impuesto" class="form-label">Impuesto</label>
                         <input id="impuesto" name="impuesto" type="number" class="form-control" tabindex="1" value="0.18">
                     </div>
                     <div class="form-group">
-                        <label for="" class="form-label">Serie Comprobante</label>
+                        <label for="serie_comprobante" class="form-label">Serie Comprobante</label>
                         <input id="serie_comprobante" name="serie_comprobante" type="text" class="form-control" tabindex="1">
                     </div>
                 </div>
                 <div class="col-md-6">
                     <div class="form-group">
-                        <label for="" class="form-label">Fecha</label>
+                        <label for="fecha_hora" class="form-label">Fecha</label>
                         <input id="fecha_hora" name="fecha_hora" type="date" class="form-control" tabindex="1">
                     </div>
                     <div class="form-group">
-                        <label for="" class="form-label">Tipo de Comprobante</label>
-                        <select class="form-control">
+                        <label for="tipo_comprobante" class="form-label">Tipo de Comprobante</label>
+                        <select class="form-control" id="tipo_comprobante" name="tipo_comprobante">
                             <option selected>Seleccionar</option>
                             <option value="BOLETA">Boleta</option>
                             <option value="FACTURA">Factura</option>
                         </select>
                     </div>
                     <div class="form-group">
-                        <label for="" class="form-label">Numero Comprobante</label>
-                        <input id="numero_comprobante" name="numero_comprobante" type="text" class="form-control" tabindex="1">
+                        <label for="num_comprobante" class="form-label">Numero Comprobante</label>
+                        <input id="num_comprobante" name="num_comprobante" type="text" class="form-control" tabindex="1">
                     </div>
                 </div>
             </div>
@@ -49,24 +56,24 @@
             <div class="row">
                 <div class="col-md-6">
                     <div class="form-group">
-                        <label for="" class="form-label">Productos</label>
-                        <select class="form-control" name="idproducto">
+                        <label for="idproducto" class="form-label">Productos</label>
+                        <select class="form-control" name="idproducto" id="idproducto">
                             <option selected>Seleccionar</option>
-                            @foreach ($producto as $produc)
-                                <option value="{{$produc->id}}">{{$produc->nombre}}</option>
+                            @foreach ($productos as $producto)
+                                <option value="{{$producto->id}}">{{$producto->nombre}}</option>
                             @endforeach
                         </select>
                     </div>
                 </div>
                 <div class="col-md-3">
                     <div class="form-group">
-                        <label for="" class="form-label">Cantidad</label>
+                        <label for="cantidad" class="form-label">Cantidad</label>
                         <input id="cantidad" name="cantidad" type="number" class="form-control" tabindex="1">
                     </div>
                 </div>
                 <div class="col-md-3">
                     <div class="form-group">
-                        <label for="" class="form-label">Precio</label>
+                        <label for="precio" class="form-label">Precio</label>
                         <input id="precio" name="precio" type="number" class="form-control" tabindex="1" value="0.00">
                     </div>
                 </div>
@@ -84,7 +91,7 @@
                             <th>Producto</th>
                             <th>Precio</th>
                             <th>Cantidad</th>
-                            <th>SubTotal (CLP)</th>
+                            <th>SubTotal $</th>
                         </tr>
                         </thead>
                         <tfoot>
@@ -93,16 +100,16 @@
                                 <p align="right">TOTAL:</p>
                             </th>
                             <th>
-                                <p align="right"><span id="total">CLP 0.00</span>
+                                <p align="right"><span id="total">$ 0.00</span>
                                 </p>
                             </th>
                         </tr>
                         <tr>
                             <th colspan="4">
-                                <p align="right">TOTAL IMPUESTO (19%)</p>
+                                <p align="right">TOTAL IMPUESTO (18%)</p>
                             </th>
                             <th>
-                                <p align="right"><span id="total_impuesto">CLP 0.00</span>
+                                <p align="right"><span id="total_impuesto">$ 0.00</span>
                                 </p>
                             </th>
                         </tr>
@@ -111,7 +118,7 @@
                                 <p align="right">TOTAL A PAGAR</p>
                             </th>
                             <th>
-                                <p align="right"><span align="right" id="total_pagar_html">CLP 0.00</span>
+                                <p align="right"><span align="right" id="total_pagar_html">$ 0.00</span>
                                     <input type="hidden" name="total" id="total_pagar">
                                 </p>
                             </th>
@@ -130,10 +137,90 @@
     </form>
 @stop
 
-@section('css')
-    <link rel="stylesheet" href="/css/admin_custom.css">
-@stop
+
 
 @section('js')
-
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script>
+        $(document).ready(function () {
+            $("#agregar").click(function () {
+                agregar();
+            });
+        });
+        var cont = 0;
+        total = 0;
+        subtotal = [];
+        $("#guardar").hide();
+        function agregar() {
+            idproducto = $("#idproducto").val();
+            producto = $("#idproducto option:selected").text();
+            cantidad = $("#cantidad").val();
+            precio = $("#precio").val();
+            impuesto = $("#impuesto").val();
+            if (idproducto != "" && cantidad != "" && cantidad > 0 && precio != "") {
+                subtotal[cont] = cantidad * precio;
+                total = total + subtotal[cont];
+                var fila = '<tr class="selected" id="fila'+cont+'"><td><button type="button" class="btn btn-danger btn-sm" onclick="eliminar('+cont+');"><i class="fa fa-times"></i></button></td> <td><input type="hidden" name="idproducto[]" value="'+idproducto+'">'+producto+'</td> <td> <input type="hidden" id="precio[]" name="precio[]" value="' + precio + '"> <input class="form-control" type="number" id="precio[]" value="' + precio + '" disabled> </td>  <td> <input type="hidden" name="cantidad[]" value="' + cantidad + '"> <input class="form-control" type="number" value="' + cantidad + '" disabled> </td> <td align="right">$' + subtotal[cont] + ' </td></tr>';
+                /*'<tr class="selected" id="fila'+cont+'">' +
+                '   <td>' +
+                '       <button type="button" class="btn btn-danger btn-sm" onclick="eliminar('+cont+');">' +
+                '           <i class="fa fa-times"></i>' +
+                '       </button>' +
+                '   </td>' +
+                '   <td>' +
+                '       <input type="hidden" name="product_id[]" value="'+product_id+'">'+producto+'' +
+                '   </td> ' +
+                '   <td>' +
+                '       <input type="hidden" id="price[]" name="price[]" value="' + price + '"> ' +
+                '       <input class="form-control" type="number" id="price[]" value="' + price + '" disabled> ' +
+                '   </td>' +
+                '   <td> ' +
+                '       <input type="hidden" name="quantity[]" value="' + quantity + '">' +
+                '       <input class="form-control" type="number" value="' + quantity + '" disabled>' +
+                '   </td>' +
+                '   <td align="right">s/' + subtotal[cont] + ' </td>' +
+                '</tr>';*/
+                cont++;
+                limpiar();
+                totales();
+                evaluar();
+                $('#detalles').append(fila);
+            } else {
+                Swal.fire({
+                    type: 'error',
+                    text: 'Rellene todos los campos del detalle de la compra',
+                })
+            }
+        }
+        function limpiar() {
+            $("#cantidad").val("");
+            $("#precio").val("");
+        }
+        function totales() {
+            $("#total").html("$ " + total.toFixed(2));
+            total_impuesto = total * impuesto;
+            total_pagar = total + total_impuesto;
+            $("#total_impuesto").html("$ " + total_impuesto.toFixed(2));
+            $("#total_pagar_html").html("$ " + total_pagar.toFixed(2));
+            $("#total_pagar").val(total_pagar.toFixed(2));
+        }
+        function evaluar() {
+            if (total > 0) {
+                $("#guardar").show();
+            } else {
+                $("#guardar").hide();
+            }
+        }
+        function eliminar(index) {
+            total = total - subtotal[index];
+            total_impuesto = total * impuesto;
+            total_pagar_html = total + total_impuesto;
+            $("#total").html("CLP" + total);
+            $("#total_impuesto").html("CLP" + total_impuesto);
+            $("#total_pagar_html").html("CLP" + total_pagar_html);
+            $("#total_pagar").val(total_pagar_html.toFixed(2));
+            $("#fila" + index).remove();
+            evaluar();
+        }
+    </script>
 @stop
